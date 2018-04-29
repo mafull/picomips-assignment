60729abb2d7501fe22533942ac7c57c97767d0cc
Max Fuller <max.fuller01@btinternet.com>
2018-04-29 01:27:09 +0100

    Add affine test for MATLAB

---
+++ matlab/affine.m
@@ -0,0 +1,22 @@
+function [x2,y2] = affine(x1,y1)
+    A = [
+            0.75, 0.5;
+            -0.5, 0.75
+        ];
+
+    B = [
+            20;
+            -20
+        ];
+    
+    % Multiplications + rounding
+    res1 = fix(A(1,1) * x1);
+    res2 = fix(A(1,2) * y1);
+    res3 = fix(A(2,1) * x1);
+    res4 = fix(A(2,2) * y1);
+    
+    % Additions    
+    x2 = res1 + res2 + B(1);
+    y2 = res3 + res4 + B(2);
+
+
