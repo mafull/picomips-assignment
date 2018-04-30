560301629618fd4252b59af441243834f38be052
Max Fuller <max.fuller01@btinternet.com>
2018-05-01 00:39:34 +0100

    Remove cpu module, further clean-up

--- implementation/sv/picoMIPS.sv
+++ implementation/sv/picoMIPS.sv
@@ -1,42 +1,95 @@
 module picoMIPS(
 	// Board outputs
 	output wire [7:0] LED,
-	output wire [6:0] SEG4,
+	//output wire [6:0] SEG4,
 
 	// Board inputs
-	//input wire [3:0] BUTTON,
 	input wire [9:0] SW,
 
 	// Clock
-	input wire clk//, nRst
+	input wire clk
 );
 
-	wire [(cpuConfig::P_SIZE-1):0] displayPC;
-
-
-	cpu
-		#(
-			.N(cpuConfig::N),
-			.A_SIZE(cpuConfig::A_SIZE),
-			.O_SIZE(cpuConfig::O_SIZE),
-			.P_SIZE(cpuConfig::P_SIZE),
-			.R_SIZE(cpuConfig::R_SIZE)
-		) cpu_inst
-		(
-			.displayResult(LED[(cpuConfig::N-1):0]),
-			.displayPC(displayPC),
-
-			.switchesIn(SW),
-			
-			.clk(clk)//,
-			//.nRst(nRst)
-		);
-
+	//wire [(cpuConfig::P_SIZE-1):0] displayPC;
 
 	/*sevenSegment seg4(
 		SEG4,
 		displayPC[3:0],
 		1'b1
 	);*/
+
+	// Parameters
+	parameter N = cpuConfig::N;
+	parameter A_SIZE = cpuConfig::A_SIZE;
+	parameter O_SIZE = cpuConfig::O_SIZE;
+	parameter P_SIZE = cpuConfig::P_SIZE;
+	parameter R_SIZE = cpuConfig::R_SIZE;
+
+
+	// Control lines
+	wire writeReg;
+	cpuConfig::aluFunc_t aluFunc;
+	wire aluImmediate;
+	wire immSwitches;
+
+	// Operands
+	wire [(R_SIZE-1):0] opD;
+	wire [(N-1):0] opS;
+
+
+	// Control path instance
+	controlPath
+		#(
+			.N(N),
+			.A_SIZE(A_SIZE),
+			.O_SIZE(O_SIZE),
+			.P_SIZE(P_SIZE),
+			.R_SIZE(R_SIZE)
+		) cp
+		(
+			// Demo
+			.displayPC(),
+			.switchesIn(SW),
+
+			// Outputs
+			// Control
+			.writeReg(writeReg),
+			.aluFunc(aluFunc),
+			.aluImmediate(aluImmediate),
+			.immSwitches(immSwitches),
+			//Data
+			.opD(opD),
+			.opS(opS),
+
+			// Clock
+			.clk(clk)
+		);
+
+
+	// Data path instance
+	dataPath
+		#(
+			.N(N),
+			.A_SIZE(A_SIZE),
+			.R_SIZE(R_SIZE)
+		) dp
+		(
+			// Demo
+			.displayResult(LED),
+			.switchesIn(SW),
+
+			// Inputs
+			// Control
+			.writeReg(writeReg),
+			.aluFunc(aluFunc),
+			.aluImmediate(aluImmediate),
+			.immSwitches(immSwitches),
+			// Data
+			.opD(opD),
+			.opS(opS),
+
+			// Clock
+			.clk(clk)
+		);
 
 endmodule
--- implementation/sv/picoMIPS4test.sv
+++ implementation/sv/picoMIPS4test.sv
@@ -1,18 +1,16 @@
 // synthesise to run on Altera DE0 for testing and demo
 module picoMIPS4test(
-    input logic fastclk,  // 50MHz Altera DE0 clock
-    input logic [9:0] SW, // Switches SW0..SW9
-    //input logic [3:0] BUTTON,
-    output logic [7:0] LED/*,
-    output wire [6:0] SEG4*/); // LEDs
-    
-    logic clk; // slow clock, about 10Hz
-    
-    counter c (.fastclk(fastclk),.clk(clk)); // slow clk from counter
-    
-    // to obtain the cost figure, synthesise your design without the counter 
-    // and the picoMIPS4test module using Cyclone IV E as target
-    // and make a note of the synthesis statistics
-    picoMIPS myDesign (.clk(clk), /*.nRst(BUTTON[0]), */.SW(SW), .LED(LED)/*, .SEG4(SEG4)*/);
-    
-  endmodule  
+  input logic fastclk,  // 50MHz Altera DE0 clock
+  input logic [9:0] SW, // Switches SW0..SW9
+  output logic [7:0] LED); // LEDs
+  
+  logic clk; // slow clock, about 10Hz
+  
+  counter c (.fastclk(fastclk),.clk(clk)); // slow clk from counter
+  
+  // to obtain the cost figure, synthesise your design without the counter 
+  // and the picoMIPS4test module using Cyclone IV E as target
+  // and make a note of the synthesis statistics
+  picoMIPS myDesign (.clk(clk), .SW(SW),.LED(LED));
+  
+endmodule  
