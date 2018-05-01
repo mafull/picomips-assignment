df3bcb2396751d864e2b3334fd799461121c8872
Max Fuller <max.fuller01@btinternet.com>
2018-05-01 23:02:16 +0100

    Update matlab file and add screenshots

--- matlab/affine.m
+++ matlab/affine.m
@@ -1,4 +1,4 @@
-function [x2,y2] = affine(x1,y1)
+function affine(x1,y1)
     A = [
             0.75, 0.5;
             -0.5, 0.75
@@ -9,6 +9,10 @@ function [x2,y2] = affine(x1,y1)
             -20
         ];
     
+    % Display x1 and y1 (decimal and binary)
+    disp(['x1 = ' num2str(x1) ' (' num2str(dec2bin(typecast(int8(x1),'uint8'),8)) ')']);
+    disp(['y1 = ' num2str(y1) ' (' num2str(dec2bin(typecast(int8(y1),'uint8'),8)) ')']);
+    
     % Multiplications + rounding
     res1 = floor(A(1,1) * x1);
     res2 = floor(A(1,2) * y1);
@@ -18,5 +22,8 @@ function [x2,y2] = affine(x1,y1)
     % Additions
     x2 = res1 + res2 + B(1);
     y2 = res3 + res4 + B(2);
-
+    
+    % Display x2 and y2 (decimal and binary)
+    disp(['x2 = ' num2str(x2) ' (' num2str(dec2bin(typecast(int8(x2),'uint8'),8)) ')']);
+    disp(['y2 = ' num2str(y2) ' (' num2str(dec2bin(typecast(int8(y2),'uint8'),8)) ')']);
 
